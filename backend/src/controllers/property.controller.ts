441cdf28f996e1fe88728aca2f48faa010f9b7e3
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-12-22 12:12:57 -0300

    add: updated and delete property by id controller

--- backend/src/controllers/property.controller.ts
+++ backend/src/controllers/property.controller.ts
@@ -1,7 +1,10 @@
 import { PropertiesFilter } from "../interfaces/filterproperties.interface";
 import { Property } from "../model/property.model";
 import { Request, Response } from "express";
-import { PropertyValidator } from "../validator/property.validator";
+import { PropertyValidator, PropertyValidatorPartial } from "../validator/property.validator";
+import mongoose from "mongoose";
+import fs from 'node:fs';
+import path from "node:path";
 
 class PropertyController {
   static getProperties = async (req: Request, res: Response): Promise<Response | void> => {
@@ -52,6 +55,92 @@ class PropertyController {
       await property.save();
 
       return res.status(201).json({ success: true, message: 'Propiedad creada correctamente', data: property });
+
+    } catch (error) {
+      const e = error as Error
+      console.log(error)
+      return res.status(500).json({ success: false, error: e.message });
+    }
+  }
+
+  static deleteProperty = async (req: Request, res: Response): Promise<Response | void> => {
+    try {
+      const { id } = req.params;
+
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.json({ success: false, error: 'El ID de la propiedad es inválido.' });
+      }
+
+      const property = await Property.findByIdAndDelete(id);
+
+      if (!property) {
+        return res.json({ success: false, error: 'Error al eliminar la propiedad.' });
+      }
+
+      if (property.image && property.image.length > 0) {
+        for (const imagePath of property.image) {
+          try {
+            fs.unlinkSync(path.resolve(imagePath))
+          } catch (error) {
+            console.log(`Error al eliminar las imagenes ${imagePath}`, error);
+          }
+        }
+      }
+
+      return res.json({ success: true, message: 'Propiedad eliminada correctamente.', data: property });
+    } catch (error) {
+      const e = error as Error
+      console.log(error)
+      return res.status(500).json({ success: false, error: e.message });
+    }
+
+  }
+
+  static updateProperty = async (req: Request, res: Response): Promise<Response | void> => {
+    try {
+      const { id } = req.params;
+      const images = req.files as Express.Multer.File[] || [];
+
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.json({ success: false, error: 'El ID de la propiedad es inválido.' });
+      }
+
+
+
+      const property = await Property.findById(id);
+
+      if (!property) {
+        return res.json({ success: false, error: 'Error al editar la propiedad.' });
+      }
+
+      if (property.image && property.image.length > 0) {
+
+        for (const oldImg of property.image) {
+          try {
+            fs.unlinkSync(path.join(oldImg))
+          } catch (error) {
+            console.log(`Error al editar las imagenes ${oldImg}`, error);
+          }
+        }
+      }
+
+      const imagesPath = images.map(img => img.path);
+
+      const validator = PropertyValidatorPartial.parse({
+        ...req.body,
+        price: Number(req.body.price),
+        baths: Number(req.body.baths),
+        rooms: Number(req.body.rooms),
+        image: imagesPath
+      })
+
+      const updatedProperty = await Property.findByIdAndUpdate(id, validator, { new: true });
+
+      if (!updatedProperty) {
+        return res.json({ success: false, error: 'Error el editar la propiedad.' })
+      }
+
+      return res.status(200).json({ success: true, message: 'Propiedad editatada correctamente.', data: updatedProperty })
 
     } catch (error) {
       const e = error as Error
