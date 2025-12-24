2df79a203ada188e28fd9003f135c4be6603b4d0
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-12-24 12:24:16 -0300

    add: frontend first files and configs

---
+++ frontend/src/routes/RoutingApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const RoutingApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+
+
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export { RoutingApp }
